16c773c37eede1005ac7156a43297266c3e7b398
Scott English <scott.h.english@gmail.com>
2021-07-26 19:47:49 -0500

    working version

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -24,18 +24,13 @@ from locust import HttpLocust, TaskSet, task
 class MetricsTaskSet(TaskSet):
     _deviceid = None
 
-    def on_start(self):
-        self._deviceid = str(uuid.uuid4())
+    @task()
+    def homepage(self):
+        self.client.get('/')
 
-    @task(1)
-    def login(self):
-        self.client.post(
-            '/login', {"deviceid": self._deviceid})
-
-    @task(999)
-    def post_metrics(self):
-        self.client.post(
-            "/metrics", {"deviceid": self._deviceid, "timestamp": datetime.now()})
+    @task()
+    def cocktailCakes(self):
+        self.client.get('/cocktail-cakes')
 
 
 class MetricsLocust(HttpLocust):
